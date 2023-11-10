84c74426de66440263b69729cef68c80b06632bf
Astg582 <109224508+Astg582@users.noreply.github.com>
2023-11-10 18:11:51 +0400

    Add files via upload

--- matr.cpp
+++ matr.cpp
@@ -1,31 +1,72 @@
 #include<iostream>
 
+const int s = 4;
 void fill(int arr[][4], const int s);
 void print(int (*arr)[4], const int s);
 int gdi(int arr[][4], const int s);
 int odi(int arr[][4], const int s);
+void priOd(int arr[][s], const int s);
+void xa(int arr[][4], int arr2[][4], const int s);
 
 int main(){
-const int s = 4;
 int arr[s][s];
-int arr1[s][s];
+int arr1[s][s] = {0};
 
 fill(arr, s);
 print(arr, s);
-std::cout << odi(arr, s); 
+std::cout << std::endl;
+xa(arr, arr1, s);
+print(arr1, s); 
 }
 
-void xa(int arr[][4], int arr2[][4], const int s){
-		for(int i = 0; i < s; ++i){
-			for(int j = 0; j < s; ++j){
-				if((j - 1 > -1) && (i - 1 > -1) && (i + 1 < s) && (j + 1 < s)){
-					arr2[i][j] = arr[i-1][j] + arr[i + 1][j] + arr[i][j - 1] + arr[i][j + 1];							   }
-				if((j - 1 < 0) && (i - 1 < 0)){
-					arr2[i][j] = arr[i + 1][j] + arr[i][j +1];
-				} 
-				if(i - 1 < 0 && j_)  
+void priOd(int arr[][s], const int s){
+for(int i = 0; i < s; ++i){
+	std::cout << std::endl;
+	for(int j = 0; j < s; ++j){
+		if(j < s - i - 1){
+		std::cout << "   ";
 		}
+		if((j + i) >= s-1){
+		std::cout << arr[i][j] << ' ';
+		}
+	}    
+	
 }
+} 
+
+
+
+void xa(int arr[][4], int arr2[][4], const int s){
+	for(int i = 0; i < s; ++i){
+		for(int j = 0; j < s; ++j){
+			if(i == 0){
+				if(j == 0){
+					arr2[i][j] = arr[i + 1][j] + arr[i][j + 1];	
+				}else if(j == s - 1){
+					arr2[i][j] = arr[i + 1][j] + arr[i][j - 1];
+				}else{
+					arr2[i][j] = arr[i][j - 1] + arr[i][j + 1] + arr[i + 1][j];
+				}	
+			}else if((j == s - 1) && (i != s - 1) && (i != 0)){
+				arr2[i][j] = arr[i - 1][j] + arr[i + 1][j] + arr[i][j -1];
+			}else if((j == 0) && (i != s -1)){
+				arr2[i][j] = arr[i + 1][j] + arr[i][j + 1] + arr[i - 1][j];
+			}else if(i == s - 1){
+				if(j == s - 1){
+					arr2[i][j] = arr[i-1][j] + arr[i][j - 1];
+				}else if( j - 1 < 0){
+					arr2[i][j] = arr[i-1][j] + arr[i][j + 1];
+				}else{
+					arr2[i][j] = arr[i][j - 1] + arr[i - 1][j] + arr[i][j + 1];
+				}
+			}else{
+				arr2[i][j] = arr[i-1][j] + arr[i+1][j] + arr[i][j -1] + arr[i][j+1]; 
+			}
+			
+		}
+	}
+}
+
 
 int odi(int arr[][4], const int s){
 	int a = 0;
